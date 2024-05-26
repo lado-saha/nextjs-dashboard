a14d37f94e459736650cfc5554bf907c40bab6e3
sih <ladokihosaha@gmail.com>
2024-05-26 18:41:54 +0100

    Added font and Image loading. Always add image sizes(mobile or desktop)

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -4,7 +4,9 @@ import clsx from 'clsx';
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
+      className={
+        // TODO: This is for conditional Styling
+        clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
